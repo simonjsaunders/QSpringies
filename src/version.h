06dffe6cf11135c01136ed33df8c9d5aa4e004ed
Simon Saunders <simon.saunders@ntlworld.com>
2022-03-26 15:19:15 +0000

    Version 1.0.1

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_NUMBER "1.0"
-#define COPYRIGHT_YEAR "2020"
+#define VERSION_NUMBER "1.0.1"
+#define COPYRIGHT_YEAR "2022"
 
 #endif // VERSION_H
